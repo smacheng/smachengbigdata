f9cfe250889829efddd5eff5e3f3b80d724882b3
gq <gq@LAPTOP-T0UCKM14>
2017-07-05 09:46:20 +0800

    simple rpc completed

--- bigdata/src/main/scala/com/smacheng/bigdata/scala/akka/rpc/Master.scala
+++ bigdata/src/main/scala/com/smacheng/bigdata/scala/akka/rpc/Master.scala
@@ -1,51 +1,81 @@
 package com.smacheng.bigdata.scala.akka.rpc
 
-import akka.actor.{ Actor, ActorSystem, Props }
+import akka.actor.{Actor, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
-/**
- * Created by smacheng on 2017-06-29.
- */
-class Master extends Actor {
+import scala.collection.mutable
+import scala.concurrent.duration._
 
-  println("constructor invoked")
+/**
+  * Created by root on 2016/5/13.
+  */
+class Master(val host: String, val port: Int) extends Actor {
+
+  // workerId -> WorkerInfo
+  val idToWorker = new mutable.HashMap[String, WorkerInfo]()
+  // WorkerInfo
+  val workers = new mutable.HashSet[WorkerInfo]() //使用set删除快, 也可用linkList
+  //超时检查的间隔
+  val CHECK_INTERVAL = 15000
+
 
   override def preStart(): Unit = {
     println("preStart invoked")
+    //导入隐式转换
+    import context.dispatcher //使用timer太low了, 可以使用akka的, 使用定时器, 要导入这个包
+    context.system.scheduler.schedule(0 millis, CHECK_INTERVAL millis, self, CheckTimeOutWorker)
   }
 
+  // 用于接收消息
   override def receive: Receive = {
-    case "connect" => {
-      println("a client connected")
-      sender ! "reply"
+    case RegisterWorker(id, memory, cores) => {
+      //判断一下，是不是已经注册过
+      if(!idToWorker.contains(id)){
+        //把Worker的信息封装起来保存到内存当中
+        val workerInfo = new WorkerInfo(id, memory, cores)
+        idToWorker(id) = workerInfo
+        workers += workerInfo
+        sender ! RegisteredWorker(s"akka.tcp://MasterSystem@$host:$port/user/Master")//通知worker注册
+      }
+    }
+    case Heartbeat(id) => {
+      if(idToWorker.contains(id)){
+        val workerInfo = idToWorker(id)
+        //报活
+        val currentTime = System.currentTimeMillis()
+        workerInfo.lastHeartbeatTime = currentTime
+      }
     }
 
-    case "hello" => {
-      println("hello")
+    case CheckTimeOutWorker => {
+      val currentTime = System.currentTimeMillis()
+      val toRemove = workers.filter(x => currentTime - x.lastHeartbeatTime > CHECK_INTERVAL)
+      for(w <- toRemove) {
+        workers -= w
+        idToWorker -= w.id
+      }
+      println(workers.size)
     }
   }
 }
 
 object Master {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
 
     val host = args(0)
     val port = args(1).toInt
-
+    // 准备配置
     val configStr =
       s"""
-        |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
-        |akka.remote.netty.tcp.hostname = "$host"
-        |akka.remote.netty.tcp.port = "$port"
-        """.stripMargin
+         |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
+         |akka.remote.netty.tcp.hostname = "$host"
+         |akka.remote.netty.tcp.port = "$port"
+       """.stripMargin
     val config = ConfigFactory.parseString(configStr)
+    //ActorSystem老大，辅助创建和监控下面的Actor，他是单例的
     val actorSystem = ActorSystem("MasterSystem", config)
-
-    val master = actorSystem.actorOf(Props[Master], "Master")
-
-    master ! "hello"
-
+    //创建Actor
+    val master = actorSystem.actorOf(Props(new Master(host, port)), "Master")
     actorSystem.awaitTermination()
-
   }
 }
--- bigdata/src/main/scala/com/smacheng/bigdata/scala/akka/rpc/Worker.scala
+++ bigdata/src/main/scala/com/smacheng/bigdata/scala/akka/rpc/Worker.scala
@@ -1,47 +1,63 @@
 package com.smacheng.bigdata.scala.akka.rpc
 
-import akka.actor.{ Actor, ActorSelection, ActorSystem, Props }
+import java.util.UUID
+
+import akka.actor.{Actor, ActorSelection, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration._
 
 /**
- * Created by smacheng on 2017-06-29.
- */
-class Worker(val masterHost: String, val masterPort: Int) extends Actor {
+  * Created by root on 2016/5/13.
+  */
+class Worker(val masterHost: String, val masterPort: Int, val memory: Int, val cores: Int) extends Actor{
 
-  var master: ActorSelection = _
+  var master : ActorSelection = _
+  val workerId = UUID.randomUUID().toString
+  val HEART_INTERVAL = 10000
 
+  //
   override def preStart(): Unit = {
+    //跟Master建立连接
     master = context.actorSelection(s"akka.tcp://MasterSystem@$masterHost:$masterPort/user/Master")
-
-    master ! "connect"
+    //向Master发送注册消息
+    master ! RegisterWorker(workerId, memory, cores)
   }
 
   override def receive: Receive = {
-    case "reply" => {
-      println("a reply from Master")
+    case RegisteredWorker(masterUrl) => {
+      println(masterUrl)
+      //启动定时器发送心跳
+      import context.dispatcher
+      //多长时间后执行 单位,多长时间执行一次 单位, 消息的接受者(直接给master发不好, 先给自己发送消息, 以后可以做下判断, 什么情况下再发送消息), 信息
+      context.system.scheduler.schedule(0 millis, HEART_INTERVAL millis, self, SendHeartbeat)
+    }
+
+    case SendHeartbeat => {
+      println("send heartbeat to master")
+      master ! Heartbeat(workerId)
     }
   }
 }
 
 object Worker {
-  def main(args: Array[String]): Unit = {
-
+  def main(args: Array[String]) {
     val host = args(0)
     val port = args(1).toInt
     val masterHost = args(2)
     val masterPort = args(3).toInt
-
+    val memory = args(4).toInt
+    val cores = args(5).toInt
+    // 准备配置
     val configStr =
       s"""
-        |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
-        |akka.remote.netty.tcp.hostname = "$host"
-        |akka.remote.netty.tcp.port = "$port"
-        """.stripMargin
+         |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
+         |akka.remote.netty.tcp.hostname = "$host"
+         |akka.remote.netty.tcp.port = "$port"
+       """.stripMargin
     val config = ConfigFactory.parseString(configStr)
+    //ActorSystem老大，辅助创建和监控下面的Actor，他是单例的
     val actorSystem = ActorSystem("WorkerSystem", config)
-
-    actorSystem.actorOf(Props(new Worker(masterHost, masterPort)), "Worker")
-
+    actorSystem.actorOf(Props(new Worker(masterHost, masterPort, memory, cores)), "Worker")
     actorSystem.awaitTermination()
   }
-}  
+}
